5155b5b30d8e6c332bc32f479fba85a8fe1d7e2a
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-12-29 15:07:51 +0530

    mb n

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Component/Login/Login";
+import Dashboard from "./Component/Dashboard/Dashboard";
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
